401d2291d373b78340608d6d69d03fa628bcadcb
Haris Fauzi <haris.fauzi@gmail.com>
2022-07-05 02:37:06 +1000

    Removed arn output

--- modules/aws/transit_gateway/aws_ec2_transit_gateway_route/outputs.tf
+++ modules/aws/transit_gateway/aws_ec2_transit_gateway_route/outputs.tf
@@ -2,8 +2,3 @@ output "id" {
   description = "The ID of the Flow Log"
   value       = try(aws_ec2_transit_gateway_route.this[0].id, "")
 }
-
-output "arn" {
-  description = "The ARN of the Flow Log"
-  value       = try(aws_ec2_transit_gateway_route.this[0].arn, "")
-}
