af8ce2707ab9d75007d6eb24fc3cabc43033199e
Haris Fauzi <haris.fauzi@gmail.com>
2022-07-05 02:58:37 +1000

    Fixed outputs

--- modules/aws/transit_gateway/aws_ec2_transit_gateway_route_table_propagation/outputs.tf
+++ modules/aws/transit_gateway/aws_ec2_transit_gateway_route_table_propagation/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "EC2 Transit Gateway Route Table identifier combined with EC2 Transit Gateway Attachment identifier."
-  value       = try(aws_ec2_transit_gateway_route_table_association.this[0].id, "")
+  value       = try(aws_ec2_transit_gateway_route_table_propagation.this[0].id, "")
 }
 
 output "resource_id" {
   description = "Identifier of the resource."
-  value       = try(aws_ec2_transit_gateway_route_table_association.this[0].resource_id, "")
+  value       = try(aws_ec2_transit_gateway_route_table_propagation.this[0].resource_id, "")
 }
 
 output "resource_type" {
   description = "Type of the resource."
-  value       = try(aws_ec2_transit_gateway_route_table_association.this[0].resource_type, "")
+  value       = try(aws_ec2_transit_gateway_route_table_propagation.this[0].resource_type, "")
 }
